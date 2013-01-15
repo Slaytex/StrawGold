c2606498edc57b93eb4acf0db8b17136bd5ed468
Slaytex <chris@chrisjacobs.com>
2013-01-15 10:29:05 -0800

    Created directories

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+	$(".project").hover(function(){
+		// Clone project image
+		// Append to current div
+		// Animate proportions
+		// Slide up text
+	},function(){
+		// Slide down text
+		// Animate back to original proportions
+		// Remove appended div
+	});
+});
